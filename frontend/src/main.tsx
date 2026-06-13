ee29da5183f73dff1b017aa199ea24747b4b6fc5
Uttkarsh Bartwal <23175606@student.curtin.edu.au>
2026-06-13 20:47:57 +0800

    removed the importing of index

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { QueryClient,  QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './context/AuthContext.tsx'
